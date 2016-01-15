fd54093dc8e61541409570ca28ef5c849d5bef76
MiguelAngelGomezHidalgo <miguel15102008@hotmail.com>
2016-01-15 14:08:01 -0600

    sin descripcion

---
+++ src/Saludo.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+/**
+ * 
+ * @author Miguel Angel Gomez Hidalgo
+ *
+ */
+public class Saludo {
+/**
+ * 
+ * @param metodo void con argumento del tipo String
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				JOptionPane.showMessageDialog(null, "Hola Miguel");
+				System.out.println("Hola desde la consola");
+
+
+	}
+
+}
